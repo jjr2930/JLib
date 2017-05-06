cb9f73540d81cee076304b316c7e6d4fc248fffe
jjr2930 <theoowwp@naver.com>
2017-05-06 16:51:34 +0900

    라이브러리업데이트 1. Tween 2. LacalizeTable 3. GlobalEventQueue 변경 4. configure 5. UITooltip

---
+++ PCVirtualKeyMapper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+
+namespace JLib
+{
+    [Serializable]
+    public class PCVirtualKeyMapData
+    {
+        public KeyCode keyboardKey;
+        public VK_Enum virtualKey;
+    }
+    public class PCVirtualKeyMapper : MonoSingle<PCVirtualKeyMapper>
+    {
+        [SerializeField]
+        List<PCVirtualKeyMapData> mappingList
+            = new List<PCVirtualKeyMapData>();
+
+        void Update()
+        {
+            for(int i =0; i<mappingList.Count; i++)
+            {
+                if(Input.GetKeyDown(mappingList[i].keyboardKey))
+                {
+                    GlobalEventQueue.EnQueueEvent( VK_State.Down, mappingList[ i ].virtualKey );
+                }
+
+                if(Input.GetKey(mappingList[i].keyboardKey))
+                {
+                    GlobalEventQueue.EnQueueEvent(VK_State.Press, mappingList[i].virtualKey);
+                }
+
+                if(Input.GetKeyUp(mappingList[i].keyboardKey))
+                {
+                    GlobalEventQueue.EnQueueEvent( VK_State.Up, mappingList[ i ].virtualKey );
+                }
+            }
+        }
+    }
+}
---
+++ Paramters/DefaultParameters.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+/// <summary>
+/// int bool float 등과 같은 기본 자료형을 사용하기 위한 파라미터이다.
+/// 유니티의 스트럭쳐 Quaternion, Vector3도 만들어주자.
+/// </summary>
+namespace JLib
+{
+    [Serializable]
+    public class SingleParameter<T>
+    {
+        public T value;
+    }
+
+    [Serializable]
+    public class ListParameter<T> : IDisposable
+    {
+        public List<T> value;
+
+        public void Dispose()
+        {
+            for( int i = 0; i < value.Count; i++ )
+            {
+                //element dispose....
+                IDisposable element = value[ i ] as IDisposable;
+                if( null != element )
+                {
+                    element.Dispose();
+                }
+            }
+            value.Clear();
+        }
+    }
+}
