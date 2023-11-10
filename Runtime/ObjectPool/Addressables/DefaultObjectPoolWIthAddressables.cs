de6e81810fa106e8aa4562b0b55d77f3d572a970
joojungyeol <joojungyeol@gmail.com>
2023-11-10 22:47:07 +0900

    오브젝트 풀 수정, 설치 그림 추가.

--- Runtime/ObjectPool/Addressables/DefaultObjectPoolWIthAddressables.cs
+++ Runtime/ObjectPool/Addressables/DefaultObjectPoolWIthAddressables.cs
@@ -10,31 +10,31 @@ namespace JLib.ObjectPool.Addressables
     /// <summary>
     /// Object pool using addressables
     /// </summary>
-    public class DefaultObjectPoolWithAddressables : PoolBase<DefaultKey, AssetReference, DefaultPoolObject, DefaultObjectPoolWithAddressables>
+    public class DefaultObjectPoolWithAddressables<KeyT> : PoolBase<KeyT, AssetReference, DefaultPoolObject<KeyT>, DefaultObjectPoolWithAddressables<KeyT>>
     {
-        protected override void DoPoped(DefaultPoolObject popedObject)
+        protected override void DoPoped(DefaultPoolObject<KeyT> popedObject)
         {
             base.DoPoped(popedObject);
             popedObject.OnPoped();
         }
 
-        protected override void DoReturned(DefaultPoolObject returnedObject)
+        protected override void DoReturned(DefaultPoolObject<KeyT> returnedObject)
         {
             base.DoReturned(returnedObject);
             returnedObject.OnReturned();
         }
         
-        protected override DefaultPoolObject CreateInstance(AssetReference poolObject)
+        protected override DefaultPoolObject<KeyT> CreateInstance(AssetReference poolObject)
         {
             var one = poolObject.InstantiateAsync().WaitForCompletion();
-            var defaultPoolObject = one.GetComponent<DefaultPoolObject>();
+            var defaultPoolObject = one.GetComponent<DefaultPoolObject<KeyT>>();
             if (null == defaultPoolObject)
                 throw new InvalidOperationException($"{poolObject.ToString()} is null");
 
             return defaultPoolObject;
         }
 
-        protected override DefaultPoolObject InstantiateUsingInstance(DefaultPoolObject instance)
+        protected override DefaultPoolObject<KeyT> InstantiateUsingInstance(DefaultPoolObject<KeyT> instance)
         {
             return Instantiate(instance);
         }
--- Runtime/ObjectPool/DefaultObjectPool.cs
+++ Runtime/ObjectPool/DefaultObjectPool.cs
@@ -8,30 +8,30 @@ namespace JLib.ObjectPool
     /// <summary>
     /// Addressables를 사용하지 않는 오브젝트 풀
     /// </summary>
-    public class DefaultObjectPool : PoolBase<DefaultKey, GameObject, DefaultPoolObject, DefaultObjectPool>
+    public class DefaultObjectPool<KeyT> : PoolBase<KeyT, GameObject, DefaultPoolObject<KeyT>, DefaultObjectPool<KeyT>>
     {
-        protected override DefaultPoolObject CreateInstance(GameObject poolObject)
+        protected override DefaultPoolObject<KeyT> CreateInstance(GameObject poolObject)
         {
             var one = Instantiate(poolObject);
-            var defaultPoolObject = one.GetComponent<DefaultPoolObject>();
+            var defaultPoolObject = one.GetComponent<DefaultPoolObject<KeyT>>();
             if (null == defaultPoolObject)
                 throw new InvalidOperationException($"{one.name} has no DefaultPoolObject");
 
             return defaultPoolObject;
         }
 
-        protected override DefaultPoolObject InstantiateUsingInstance(DefaultPoolObject instance)
+        protected override DefaultPoolObject<KeyT> InstantiateUsingInstance(DefaultPoolObject<KeyT>  instance)
         {
             return Instantiate(instance);
         }
 
-        protected override void DoPoped(DefaultPoolObject popedObject)
+        protected override void DoPoped(DefaultPoolObject<KeyT> popedObject)
         {
             base.DoPoped(popedObject);
             popedObject.OnPoped();
         }
 
-        protected override void DoReturned(DefaultPoolObject returnedObject)
+        protected override void DoReturned(DefaultPoolObject<KeyT> returnedObject)
         {
             base.DoReturned(returnedObject);
             returnedObject.OnReturned();
