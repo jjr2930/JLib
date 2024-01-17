de3b1084c4ba3035a19ab579b7f8e73daab66bcb
joojungyeol <joojungyeol@gmail.com>
2024-01-17 15:00:30 +0900

    원래대로

--- Runtime/ObjectPool/PoolObject.cs
+++ Runtime/ObjectPool/PoolObject.cs
@@ -5,8 +5,17 @@ using UnityEngine;
 
 namespace JLib.ObjectPool
 {
-    public class PoolObject : MonoBehaviour
+    /// <summary>
+    /// 풀 오브젝트의 기본 클래스
+    /// </summary>
+    /// <typeparam name="KeyT"></typeparam>
+    [Serializable]
+    public class PoolObject<KeyT> : MonoBehaviour
     {
+        /// <summary>
+        /// 풀에서 관리될 오브젝트의 키
+        /// </summary>
+        public KeyT key;
         /// <summary>
         /// 처음 만들어졌을 때 할 행위
         /// </summary>
@@ -19,18 +28,5 @@ namespace JLib.ObjectPool
         /// 풀로 다시 들어갈 때 할 행위
         /// </summary>
         public virtual void OnReturned() { }
-    }
-
-    /// <summary>
-    /// 풀 오브젝트의 기본 클래스
-    /// </summary>
-    /// <typeparam name="KeyT"></typeparam>
-    [Serializable]
-    public class PoolObject<KeyT> : PoolObject
-    {
-        /// <summary>
-        /// 풀에서 관리될 오브젝트의 키
-        /// </summary>
-        public KeyT key;
     }
 }
