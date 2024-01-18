957853008b983f4201009ad6749538f8f237cac0
joojungyeol <joojungyeol@gmail.com>
2024-01-18 13:14:32 +0900

    주석을 제거하면 임포트가 되나?

--- Runtime/Table/TableBase.cs
+++ Runtime/Table/TableBase.cs
@@ -7,10 +7,8 @@ namespace JLib
     public class TableBase<KeyT,SchemeT> : ScriptableObject
         where SchemeT : TableSchemeBase<KeyT>
     {
-        //단지 직렬화를 위한 것일 뿐
         [SerializeField] List<SchemeT> data = new List<SchemeT>();
         
-        //사용은 이것으로
         Dictionary<KeyT, SchemeT> datumByKey;
 
         public SchemeT this[KeyT key]
